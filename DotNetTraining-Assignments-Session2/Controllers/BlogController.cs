5101f2367ad6110ba64884db88bad90761fea325
Rakesh <ladrakesh@outlook.com>
2022-09-21 16:44:36 +0530

    Added session 2

---
+++ DotNetTraining-Assignments-Session2/Controllers/BlogController.cs
@@ -0,0 +1,38 @@
+﻿using DotNetTraining_Assignments_Session2.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTraining_Assignments_Session2.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IConfiguration _config;
+        public BlogController(IConfiguration config)
+        {
+            _config = config;
+
+        }
+        
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            var person = new Person()
+            {
+                Id = 1,
+                Name = "Rakesh Lad",
+                Age = 37
+            };
+
+            var section = _config.GetSection("Logging:LogLevel");
+            var value = section.GetValue<string>("Default");
+           
+            person.Name= value;
+
+            return View(person);
+        }
+    }
+}
---
+++ DotNetTraining-Assignments-Session2/Model/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetTraining_Assignments_Session2.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
